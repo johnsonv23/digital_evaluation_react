acf9c431641f153fce9dce72b80ce9a3a001d5ac
johnson2123 <johnsonvasupalli@yahoo.com>
2026-05-05 23:25:24 +0530

    Fix App import path for Vercel build

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,5 @@
-
-
-
-
-// import ReactDOM from "react-dom/client";
-// import App from "./App";
-// import { GoogleOAuthProvider } from "@react-oauth/google";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <GoogleOAuthProvider clientId="347020397859-jblncmg03j3sffj3vias2ujrk534s546.apps.googleusercontent.com">
-//     <App />
-//   </GoogleOAuthProvider>
-// );
-
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <App />
